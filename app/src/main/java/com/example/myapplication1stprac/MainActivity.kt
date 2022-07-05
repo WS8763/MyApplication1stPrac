356336bb24f5f57943b77609fea98bd3b03239a3
anreh <anurehka@gmail.com>
2022-07-05 11:02:23 +0800

    latest

--- app/src/main/java/com/example/myapplication1stprac/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1stprac/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication1stprac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button1)
+        button.setOnClickListener { sayHi() }
+    }
+    private fun sayHi(){
+        Toast.makeText(this, "Hello Button",
+        Toast.LENGTH_SHORT).show()
     }
 }
